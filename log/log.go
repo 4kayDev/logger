d0bbd4d15f446d4a62623200d0ab1815cafd8515
slipneff <slipneff@mail.ru>
2023-11-27 20:45:35 +0300

    [Feat] Add more tags

--- log/log.go
+++ log/log.go
@@ -15,6 +15,14 @@ var Logger = zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.
 	Logger()
 
 func Warn(msg string, tag ...string) {
+	if len(tag) == 4 {
+		Logger.Warn().Str(tag[0], tag[1]).Str(tag[2],tag[3]).Msg(msg)
+		return
+	}
+	if len(tag) == 3 {
+		Logger.Warn().Str(tag[0], tag[1]).Str("SRC",tag[2]).Msg(msg)
+		return
+	}
 	if len(tag) == 2 {
 		Logger.Warn().Str(tag[0], tag[1]).Msg(msg)
 		return
@@ -27,6 +35,14 @@ func Warn(msg string, tag ...string) {
 }
 
 func Info(msg string, tag ...string) {
+	if len(tag) == 4 {
+		Logger.Info().Str(tag[0], tag[1]).Str(tag[2],tag[3]).Msg(msg)
+		return
+	}
+	if len(tag) == 3 {
+		Logger.Info().Str(tag[0], tag[1]).Str("SRC",tag[2]).Msg(msg)
+		return
+	}
 	if len(tag) == 2 {
 		Logger.Info().Str(tag[0], tag[1]).Msg(msg)
 		return
@@ -39,6 +55,14 @@ func Info(msg string, tag ...string) {
 }
 
 func Debug(msg string, tag ...string) {
+	if len(tag) == 4 {
+		Logger.Debug().Str(tag[0], tag[1]).Str(tag[2],tag[3]).Msg(msg)
+		return
+	}
+	if len(tag) == 3 {
+		Logger.Debug().Str(tag[0], tag[1]).Str("SRC",tag[2]).Msg(msg)
+		return
+	}
 	if len(tag) == 2 {
 		Logger.Debug().Str(tag[0], tag[1]).Msg(msg)
 		return
@@ -51,6 +75,14 @@ func Debug(msg string, tag ...string) {
 }
 
 func Trace(msg string, tag ...string) {
+	if len(tag) == 4 {
+		Logger.Trace().Str(tag[0], tag[1]).Str(tag[2],tag[3]).Msg(msg)
+		return
+	}
+	if len(tag) == 3 {
+		Logger.Trace().Str(tag[0], tag[1]).Str("SRC",tag[2]).Msg(msg)
+		return
+	}
 	if len(tag) == 2 {
 		Logger.Trace().Str(tag[0], tag[1]).Msg(msg)
 		return
@@ -63,6 +95,14 @@ func Trace(msg string, tag ...string) {
 }
 
 func Panic(err error, msg string, tag ...string) {
+	if len(tag) == 4 {
+		Logger.Panic().Str(tag[0], tag[1]).Str(tag[2],tag[3]).Msg(msg)
+		return
+	}
+	if len(tag) == 3 {
+		Logger.Panic().Str(tag[0], tag[1]).Str("SRC",tag[2]).Msg(msg)
+		return
+	}
 	if len(tag) == 2 {
 		Logger.Panic().Str(tag[0], tag[1]).Msg(msg)
 		return
@@ -75,24 +115,40 @@ func Panic(err error, msg string, tag ...string) {
 }
 
 func Fatal(err error, msg string, tag ...string) {
+	if len(tag) == 4 {
+		Logger.Fatal().Err(err).Str(tag[0], tag[1]).Str(tag[2],tag[3]).Msg(msg)
+		return
+	}
+	if len(tag) == 3 {
+		Logger.Fatal().Err(err).Str(tag[0], tag[1]).Str("SRC",tag[2]).Msg(msg)
+		return
+	}
 	if len(tag) == 2 {
-		Logger.Fatal().Str(tag[0], tag[1]).Msg(msg)
+		Logger.Fatal().Err(err).Str(tag[0], tag[1]).Msg(msg)
 		return
 	}
 	if len(tag) == 1 {
-		Logger.Fatal().Str("SRC", tag[0]).Msg(msg)
+		Logger.Fatal().Err(err).Str("SRC", tag[0]).Msg(msg)
 		return
 	}
 	Logger.Fatal().Msg(msg)
 }
 
 func Error(err error, msg string, tag ...string) {
+	if len(tag) == 4 {
+		Logger.Error().Err(err).Str(tag[0], tag[1]).Str(tag[2],tag[3]).Msg(msg)
+		return
+	}
+	if len(tag) == 3 {
+		Logger.Error().Err(err).Str(tag[0], tag[1]).Str("SRC",tag[2]).Msg(msg)
+		return
+	}
 	if len(tag) == 2 {
-		Logger.Error().Str(tag[0], tag[1]).Msg(msg)
+		Logger.Error().Err(err).Str(tag[0], tag[1]).Msg(msg)
 		return
 	}
 	if len(tag) == 1 {
-		Logger.Error().Str("SRC", tag[0]).Msg(msg)
+		Logger.Error().Err(err).Str("SRC", tag[0]).Msg(msg)
 		return
 	}
 	Logger.Error().Msg(msg)
